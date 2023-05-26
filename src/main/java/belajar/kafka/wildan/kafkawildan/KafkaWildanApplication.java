00d2a653571f3827c404e8b249ee4fe47ab80469
wildan <wildan@sinproworks.com>
2023-05-27 01:00:36 +0700

    kafka init

---
+++ src/main/java/belajar/kafka/wildan/kafkawildan/KafkaWildanApplication.java
@@ -0,0 +1,13 @@
+package belajar.kafka.wildan.kafkawildan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaWildanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaWildanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/belajar/kafka/wildan/kafkawildan/KafkaWildanApplicationTests.java
@@ -0,0 +1,13 @@
+package belajar.kafka.wildan.kafkawildan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaWildanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
